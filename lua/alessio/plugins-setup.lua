1e2dcd5fc623ede85adc3df9ede1481eda034f0d
Alessio Izzo <alessio.izzo86@gmail.com>
2023-06-20 23:06:29 +0200

    add treesitter playground

--- lua/alessio/plugins-setup.lua
+++ lua/alessio/plugins-setup.lua
@@ -100,6 +100,8 @@ return packer.startup(function(use)
     end,
   })
 
+  use({ "nvim-treesitter/playground" })
+
   -- auto closing
   use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
   use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
